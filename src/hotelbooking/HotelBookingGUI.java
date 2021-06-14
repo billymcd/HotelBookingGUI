da37779a80e48b8741d304fbf30c90abb45db7bd
billymcd <billymcdowd@gmail.com>
2021-06-14 14:09:04 +1200

    Start screen done
    Implemented actionListener for buttons present on the home screen. Buttons cause change in displayed central panel

--- src/hotelbooking/HotelBookingGUI.java
+++ src/hotelbooking/HotelBookingGUI.java
@@ -9,11 +9,8 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Toolkit;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -28,6 +25,7 @@ import javax.swing.SwingConstants;
  */
 public class HotelBookingGUI extends JPanel
 {
+    private HotelBooker hotBook;
     public final int PANEL_WIDTH=600;
     public final int PANEL_HEIGHT=600;
     private JPanel welcomePanel;
@@ -36,6 +34,8 @@ public class HotelBookingGUI extends JPanel
     private JPanel bookingPanel;
     private JPanel enquiryPanel;
     private JPanel northPanel;
+    private JPanel southPanel;
+    private JPanel centralPanel;
     private JButton loginButton;
     private JButton exitButton;
     private JButton acceptButton;
@@ -53,11 +53,14 @@ public class HotelBookingGUI extends JPanel
     public HotelBookingGUI()
     {
         super(new BorderLayout());
+        hotBook=new HotelBooker("PDC Hotel", "Auckland", 5);
         
         //setting up central panels; welcome, account creation/login, bookings, enquiries
+        centralPanel=new JPanel();
         welcomePanel=new JPanel();
+        welcomePanel.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
         welcomePanel.add(new JLabel("Placeholder for image"));
-        
+        ButtonListener bListener=new ButtonListener();
         eCustPanel=new JPanel();
         eCustPanel.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
         emailField=new JTextField();
@@ -70,7 +73,7 @@ public class HotelBookingGUI extends JPanel
         eCustLayout.setVerticalGroup(eCustLayout.createSequentialGroup().addGroup(eCustLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(emailField).addComponent(loginButton)));
         
         newCustPanel=new JPanel();
-        eCustPanel.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+        newCustPanel.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
         nameField=new JTextField();
         newEmailField=new JTextField();
         phoneField=new JTextField();
@@ -83,35 +86,32 @@ public class HotelBookingGUI extends JPanel
         nCustLayout.setVerticalGroup(nCustLayout.createSequentialGroup().addGroup(nCustLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(nameField).addComponent(newEmailField).addComponent(phoneField)).addComponent(acceptButton));
         
         //setting up top panel
+        northPanel=new JPanel();
         welcomeMessage=new JLabel("Welcome to the PDC Hotel Booking System");
         welcomeMessage.setHorizontalAlignment(SwingConstants.CENTER);
+        northPanel.add(welcomeMessage);
         
         //setting up bottom panel
-        JPanel southPanel=new JPanel();
+        southPanel=new JPanel();
         exitButton=new JButton("Exit");
+        exitButton.addActionListener(bListener);
         newCustomerButton=new JButton("New Customer");
+        newCustomerButton.addActionListener(bListener);
         existingCustButton=new JButton("Existing Customer");
+        existingCustButton.addActionListener(bListener);
         southPanel.add(newCustomerButton);
         southPanel.add(existingCustButton);
         southPanel.add(exitButton);
         
         //adding subpanels to primary panel
-        this.add(welcomePanel, BorderLayout.CENTER);
-        this.add(eCustPanel, BorderLayout.CENTER);
-        this.add(newCustPanel, BorderLayout.CENTER);
-        this.add(southPanel, BorderLayout.SOUTH);
-        this.add(welcomeMessage, BorderLayout.NORTH);
+        add(centralPanel, BorderLayout.CENTER);
+        centralPanel.add(welcomePanel);
+        add(southPanel, BorderLayout.SOUTH);
+        add(northPanel, BorderLayout.NORTH);
     }
     
     public static void main(String[] args) 
     {
-        String url="jdbc:derby://localhost:1527/ PDCHotel; create=true";
-        try {
-            Connection conn=DriverManager.getConnection(url, "vsf2319", "Hotel123");
-        } catch (SQLException ex) {
-            System.out.println("SQLException: "+ex.getMessage());
-        }
-        
         HotelBookingGUI hotelPanel=new HotelBookingGUI();
         JFrame frame=new JFrame("PDC Hotel Booking System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -123,5 +123,30 @@ public class HotelBookingGUI extends JPanel
         int screenWidth=d.width;
         frame.setLocation(new Point((screenWidth/2)-(frame.getWidth()/2),(screenHeight/2)-(frame.getHeight()/2)));
         frame.setVisible(true);
+    }
+    
+    private class ButtonListener implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e) 
+        {
+            if(e.getSource().equals(exitButton))
+                System.exit(0);
+            if(e.getSource().equals(newCustomerButton))
+            {
+                centralPanel.removeAll();
+                centralPanel.add(newCustPanel);
+                centralPanel.validate();
+                repaint();
+            }
+            if(e.getSource().equals(existingCustButton))
+            {
+                centralPanel.removeAll();
+                centralPanel.add(eCustPanel);
+                centralPanel.validate();
+                repaint();
+            }
+            
+        }
     }
 }
